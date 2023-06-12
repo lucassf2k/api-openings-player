a5e0a0dc9651c0ac981acf6f6c401d05d49f460a
Lucas Silva <lucassilved@gmail.com>
2023-06-12 16:43:39 -0300

    chore: add swagger config

--- src/application.ts
+++ src/application.ts
@@ -1,11 +1,15 @@
 import express from "express"
+import swaggerUi from "swagger-ui-express"
 import { routes } from "@/infra/express/routes"
 import { postgresDataSource } from "@/infra/typeorm/database/app-data-source"
 import { errorHandler } from "@/infra/express/middlewares/error-handler"
+import swaggerFile from "@/infra/config/swagger.json"
+
 function application(): void {
   const app = express()
   app.use(express.json())
-  app.use(routes)
+  app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
+  app.use("/api", routes)
   app.use(errorHandler)
   postgresDataSource
     .initialize()
