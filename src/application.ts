ed23bf9a16cf61c87d1e2e9031eb154b6b08f235
Lucas Silva <lucassilved@gmail.com>
2023-06-12 16:15:29 -0300

    chore: add database in paste typeorm

--- src/application.ts
+++ src/application.ts
@@ -1,6 +1,6 @@
 import express from "express"
 import { routes } from "@/infra/express/routes"
-import { postgresDataSource } from "@/infra/database/app-data-source"
+import { postgresDataSource } from "@/infra/typeorm/database/app-data-source"
 
 function application(): void {
   const app = express()
