38c4580abcd59830385e8b54139fc36b8e19ac3a
Lucas Silva <lucassilved@gmail.com>
2023-06-15 14:27:52 -0300

    feat: specifying request body types in the create category controller

--- src/application/controllers/category/CreateCategoryController.ts
+++ src/application/controllers/category/CreateCategoryController.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from "express"
 import { CreateCategoryUseCase } from "@/domain/usecase/category/CreateCategoryUseCase"
 import { TypeormCategoriesRepository } from "@/infra/database/typeorm/repositories/TypeormCategoriesRepository"
+import { ICreateCategoryDTO } from "@/application/dtos/ICreateCategory"
 
 export class CreateCategoryController {
   async handler(request: Request, response: Response): Promise<Response> {
-    const categoryRequest = request.body
+    const categoryRequest = request.body as ICreateCategoryDTO
     const createCategoryUseCase = new CreateCategoryUseCase(
       new TypeormCategoriesRepository(),
     )
